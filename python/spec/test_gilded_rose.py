395b0f5bfe31005ee3e740bfb5b924af5831a9e3
zerostresslvl <zerostresslvl@gmail.com>
2026-06-02 09:48:11 +0200

    changed folder structure and installed a python dependency that I knew to install thanks to copilot to test locally in my terminal

--- python/spec/test_gilded_rose.py
+++ python/spec/test_gilded_rose.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
 import unittest
 
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
+
 from gilded_rose import Item, GildedRose
+
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
@@ -9,6 +14,7 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
+
 
 if __name__ == '__main__':
     unittest.main()
--- python/spec/texttest_fixture.py
+++ python/spec/texttest_fixture.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
+
 from gilded_rose import *
+
 
 if __name__ == "__main__":
     print ("OMGHAI!")
