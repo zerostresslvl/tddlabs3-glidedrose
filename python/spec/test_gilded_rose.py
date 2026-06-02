33df0730a4f7f376e52f3f9bab1b6d87bee30e60
zerostresslvl <zerostresslvl@gmail.com>
2026-06-02 09:56:32 +0200

    cases added for items dropping by 2 doing 20 - 18 which is 2 and ends up dropping by 2 unlike with case 1 where it dropped by 1 - understood this via. explanation of what goes on in our main source file -> implemented accordingly

--- python/spec/test_gilded_rose.py
+++ python/spec/test_gilded_rose.py
@@ -15,6 +15,12 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(19, items[0].quality)
 
+    def test_normal_item_degrades_by_two_after_sell_date(self):
+        items = [Item("foo", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(18, items[0].quality)
+
 
 if __name__ == '__main__':
     unittest.main()
