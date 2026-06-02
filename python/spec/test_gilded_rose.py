8e5326800b5ce9216d2aa2155a5daa29d1284a5c
zerostresslvl <zerostresslvl@gmail.com>
2026-06-02 10:07:45 +0200

    once the 50 day mark passes for aged brie we add on a 1 so it increases by 2 instead of 1 unlike normal items - test passed

--- python/spec/test_gilded_rose.py
+++ python/spec/test_gilded_rose.py
@@ -21,6 +21,12 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(18, items[0].quality)
 
+    def test_aged_brie_increases_in_quality(self):
+        items = [Item("Aged Brie", 2, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(1, items[0].quality)
+
 
 if __name__ == '__main__':
     unittest.main()
